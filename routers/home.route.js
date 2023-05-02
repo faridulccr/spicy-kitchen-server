610b368cb358cc584a0257d327ae04c5c304f142
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-05-03 04:24:47 +0600

    send data to client

--- routers/home.route.js
+++ routers/home.route.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
+const chefs = require("../data/chefs.json");
+const recipes = require("../data/recipes.json");
 
 router.get("/", (req, res) => {
     res.status(200).json({message: "welcome to server"});
 });
 
 router.get("/chef", (req, res) => {
-    res.status(200).send();
+    res.status(200).send(chefs);
 });
 
 router.get("/recipes", (req, res) => {
-    res.status(200).send();
+    res.status(200).send(recipes);
 });
+
 module.exports = router;
