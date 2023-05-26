e73431e67deac11cbd100f21bf9c0c5cec4ec07d
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-05-26 18:53:47 +0600

    check the vercel connection

--- routers/home.route.js
+++ routers/home.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).json({message: "welcome to server"});
+    res.status(200).json({message: "welcome to spicy-kitchen-server"});
 });
 
 module.exports = router;
